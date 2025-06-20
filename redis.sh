fd8ce22c909f5e05fae3948229525494f4554906
mohan <mohanprasad390@gmail.com>
2025-06-20 23:23:20 +0530

    message

--- redis.sh
+++ redis.sh
@@ -1,39 +1,8 @@
 #!/bin/bash
 
-R="-e \e[31m"
-G="-e \e[32m"
-Y="-e \e[33m"
-N="\e[0m"
-
-LOGS_FOLDER="/var/log/roboshop-logs"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)    # $0 -> have the script name
-mkdir -p $LOGS_FOLDER
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
-SCRIPT_DIR=$PWD
-
-echo "Script started executing at : $(date)" &>>$LOG_FILE
-
-
-USERID=$(id -u)
-
-if [ $USERID -ne 0 ]
-then
-    echo $R "ERROR:: Please run this script with root access" $N | tee -a $LOG_FILE
-    exit 1 #give other than 0 upto 127
-else
-    echo $G "You are running with root access" $N | tee -a $LOG_FILE
-fi
-
-# validate functions takes input as exit status, what command they tried to install
-VALIDATE(){
-    if [ $1 -eq 0 ]
-    then
-        echo $G "$2 is ... SUCCESS" $N | tee -a $LOG_FILE
-    else
-        echo $R "$2 is ... FAILURE" $N | tee -a $LOG_FILE
-        exit 1
-    fi
-}
+source ./common.sh
+app_name=redis
+check_root
 
 dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disable default redis"
@@ -54,3 +23,5 @@ VALIDATE $? "Enabiliing redis"
 
 systemctl start redis  &>>$LOG_FILE
 VALIDATE $? "Starting redis"
+
+print_time
